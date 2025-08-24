3b8f5eaca5b3acbf81707e922100d1f3b206ce14
Marinos.Sake <protutor13@hotmail.com>
2025-08-24 22:10:00 +0300

    refactor(routing): extract routes into AppRoutes component for cleaner App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
-import {Routes, Route} from "react-router-dom";
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import Booking from "./pages/Booking.tsx";
-import Login from "./pages/Login.tsx";
-import Register from "./pages/Register.tsx";
-import MyProfile from "./pages/MyProfile.tsx";
-import DogsHotel from "./pages/DogsHotel.tsx";
-import CatHotel from "./pages/CatHotel";
-import About from "./pages/About";
-import Contact from "./pages/Contact.tsx"
+import Navbar from "./app/components/Navbar";
+import { AppRoutes } from "./app/router/Routes.tsx";
 
-
-function App() {
+export default function App() {
     return (
         <>
             <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/me" element={<MyProfile/>} />
-                <Route path="/booking" element={<Booking />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register/>}></Route>
-                <Route path="/dogs-hotel" element={<DogsHotel />} />
-                <Route path="/cats-hotel" element={<CatHotel />} />
-                <Route path="/about" element={ <About/>} />
-                <Route path="/contact" element={ <Contact/> } />
-            </Routes>
+            <AppRoutes />
         </>
     );
 }
-export default App;
